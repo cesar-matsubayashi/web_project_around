3acf922b9a9bb03c73a0e37cb77aa9bb9efbd5cb
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2024-07-19 18:16:54 -0300

    add gallery cards through form

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 const editBtn = document.querySelector(".profile__edit-btn");
-const editCloseBtn = document.querySelector(".popup-content__close-icon_edit");
+editBtn.addEventListener("click", toggleEditPopup);
 
-const addBtn = document.querySelector(".profile__add-btn");
-const addCloseBtn = document.querySelector(".popup-content__close-icon_add");
+const editCloseBtn = document.querySelector(".popup-content__close-icon_edit");
+editCloseBtn.addEventListener("click", toggleEditPopup);
 
 function toggleEditPopup() {
   const popup = document.querySelector(".popup");
@@ -25,23 +25,10 @@ function toggleEditPopup() {
   }
 }
 
-function toggleAddPopup() {
-  const popup = document.querySelector(".popup");
-  const popupContent = document.querySelector(".popup-content_add");
+const editForm = document.querySelector(".form_edit");
+editForm.addEventListener("submit", handleProfileEditFormSubmit);
 
-  popup.classList.toggle("popup_opened");
-  popupContent.classList.toggle("popup-content_opened");
-}
-
-editBtn.addEventListener("click", toggleEditPopup);
-editCloseBtn.addEventListener("click", toggleEditPopup);
-
-addBtn.addEventListener("click", toggleAddPopup);
-addCloseBtn.addEventListener("click", toggleAddPopup);
-
-const form = document.querySelector(".form");
-
-function handleProfileFormSubmit(evt) {
+function handleProfileEditFormSubmit(evt) {
   evt.preventDefault();
 
   const nameInput = document.querySelector(".form__input_el_name");
@@ -58,4 +45,72 @@ function handleProfileFormSubmit(evt) {
   toggleEditPopup();
 }
 
-form.addEventListener("submit", handleProfileFormSubmit);
+const addBtn = document.querySelector(".profile__add-btn");
+addBtn.addEventListener("click", toggleAddPopup);
+
+const addCloseBtn = document.querySelector(".popup-content__close-icon_add");
+addCloseBtn.addEventListener("click", toggleAddPopup);
+
+function toggleAddPopup() {
+  const popup = document.querySelector(".popup");
+  const popupContent = document.querySelector(".popup-content_add");
+
+  popup.classList.toggle("popup_opened");
+  popupContent.classList.toggle("popup-content_opened");
+}
+
+const gallery = document.querySelector(".gallery");
+const addForm = document.querySelector(".form_add");
+addForm.addEventListener("submit", handleProfileAddFormSubmit);
+
+function handleProfileAddFormSubmit(evt) {
+  evt.preventDefault();
+
+  const title = document.querySelector(".form__input_el_title");
+  const url = document.querySelector(".form__input_el_url");
+
+  addCard(title.value, url.value);
+  toggleAddPopup();
+}
+
+function addCard(title, url) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".gallery__card")
+    .cloneNode(true);
+
+  cardElement.querySelector(".gallery__photo").src = url;
+  cardElement.querySelector(".gallery__photo").alt = `Imagem ${title}`;
+  cardElement.querySelector(".gallery__title").textContent = title;
+
+  gallery.prepend(cardElement);
+}
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
+initialCards.forEach((card) => addCard(card.name, card.link));
