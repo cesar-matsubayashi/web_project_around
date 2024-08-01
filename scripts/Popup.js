bd8ae4f3d33f0066defd30fb0012f7b264b088e0
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2024-08-01 10:37:09 -0300

    bugfix(popup): remove close button listener

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -5,10 +5,12 @@ export default class Popup {
 
   open() {
     this._popup.classList.add("popup_is-opened");
+    console.log("Popup.open");
   }
 
   close() {
     this._popup.classList.remove("popup_is-opened");
+    // console.log("Popup.close");
   }
 
   _handleEscClose(key) {
@@ -18,18 +20,21 @@ export default class Popup {
   }
 
   setEventListeners() {
-    const closeButton = document.querySelector("popup__close");
+    // const closeButton = document.querySelector(".popup__close");
 
-    closeButton.addEventListener("click", () => {
-      this.close();
-    });
+    // closeButton.addEventListener("click", () => {
+    //   this.close();
+    //   console.log("Popup.closeButton");
+    // });
 
     this._popup.addEventListener("click", () => {
       this.close();
+      console.log("Popup.click");
     });
 
     document.addEventListener("keydown", (evt) => {
       this._handleEscClose(evt.key);
+      console.log("Popup.keydown");
     });
   }
 }
