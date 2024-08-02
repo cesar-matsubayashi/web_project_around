1838420969c3bdb67309cd1ed99e59f990be4977
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2024-08-02 11:24:31 -0300

    fix(UserInfo): querySelector input element to property

--- scripts/UserInfo.js
+++ scripts/UserInfo.js
@@ -1,19 +1,19 @@
 export default class UserInfo {
   constructor(data) {
-    this._name = data.name;
-    this._description = data.description;
+    this._name = document.querySelector(data.nameSelector);
+    this._description = document.querySelector(data.descriptionSelector);
   }
 
   getUserInfo() {
     const info = {};
-    info.name = this._name;
-    info.description = this._description;
+    info.name = this._name.textContent.trim();
+    info.description = this._description.textContent;
 
     return info;
   }
 
   setUserInfo(data) {
-    this._name = data.name;
-    this._description = data.description;
+    this._name.textContent = data.name;
+    this._description.textContent = data.description;
   }
 }
